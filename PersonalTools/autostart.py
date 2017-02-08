5aba821c5031687498e7975b7830252f8b031c30
laoluotuo <laoluotuo2500@gmail.com>
2017-02-08 19:17:38 +0800

    Update autostart.py
    添加ping功能, 测试虚拟机在线与否并决定开启动作. 修正小BUGS

--- PersonalTools/autostart.py
+++ PersonalTools/autostart.py
@@ -1,6 +1,7 @@
-#!python 3.5
+#!python3
 #coding:utf-8
-#该脚本功能是, Windows开机后探测磁盘空置率, 若高于95%, 即启动测试虚拟机(vm_ops), 然后探测虚拟磁盘G是否挂载, 若已挂载, 打开Dropbox.
+#该脚本功能: Windows开机后探测磁盘空置率, 若高于95%(足够空闲), 即启动测试虚拟机(会先ping探测虚拟机IP)
+#然后探测虚拟磁盘G是否挂载, 若已挂载, 打开Dropbox.
 import subprocess
 import psutil
 import os
@@ -11,7 +12,7 @@ import time
 vm_ops = ['C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe', 'start', 'E:\\Virtual Machines\\CentOS7.2\\CentOS7.2.vmx']
 dbx_start = ['C:\\Program Files (x86)\\Dropbox\\Client\\Dropbox.exe']
 vm_ping = ['ping', '-n', '1', '-w', '800', '192.168.116.128']
-disk_per = """typeperf "LogicalDisk(_Total)\% Idle Time" -sc 1"""
+disk_per = """typeperf "LogicalDisk(_Total)\% Idle Time" -sc 1""" #查询Windows计数器, 得到IO空闲率
 
 #processes and files
 process_dbx = 'dropbox.exe'
@@ -19,7 +20,7 @@ dbx_dir = 'g:\\dropbox'
 
 def execute(command):
     """
-    无阻塞无反馈信息执行命令
+    无输出执行命令, 无阻塞
     """
     exec_cmd = subprocess.Popen(command)
     print('Command {} excuted'.format(command))
@@ -28,7 +29,7 @@ def execute(command):
 
 def execute_full(command):
     """
-    命令执行函数, 可获取全返回信息, 可能会阻塞
+    有标准/错误输出, 可能会阻塞
     """
     exec_cmd = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return ('Command {} excuted'.format(command), exec_cmd.communicate()[0].decode('gbk'))
@@ -36,7 +37,7 @@ def execute_full(command):
 def ping(vm_ping):
     try:
         output = subprocess.check_output(vm_ping)
-        print(output)
+        print('>>>>>Ping响应:', output.decode('gbk').split('\n')[2], '\n>>>>>虚拟机已开机')
         return 0
     except Exception as e:
         print(e)
@@ -56,15 +57,17 @@ def main():
         disk_percent = execute_full(disk_per)[1].split('"')[7]
         if float(disk_percent) > 95:
             print('当前IO空闲率: {}%'.format(float(disk_percent)))
-            execute(vm_ops)
+            if ping(vm_ping) == 1:
+                execute(vm_ops)
             if isdir and not isdbx:
                 execute(dbx_start)
                 break
             elif isdir and isdbx:
-                print('Disk mounted and Dropbox is running, exit')
+                print('>>>>>Disk mounted and Dropbox is running, exit')
                 time.sleep(3)
                 break
         time.sleep(3)
+
 
 if __name__ == '__main__':
     main()
