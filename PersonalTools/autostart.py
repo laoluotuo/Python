4c01838623c6b31384426d724f468a93360d7fa0
laoluotuo <laoluotuo2500@gmail.com>
2017-02-09 09:48:29 +0800

    Update autostart.py
    美化+修改小BUG

--- PersonalTools/autostart.py
+++ PersonalTools/autostart.py
@@ -11,11 +11,13 @@ import time
 #commands
 vm_ops = ['C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe', 'start', 'E:\\Virtual Machines\\CentOS7.2\\CentOS7.2.vmx']
 dbx_start = ['C:\\Program Files (x86)\\Dropbox\\Client\\Dropbox.exe']
+keepass = ['D:\\Program Files\\Keepass\\KeePass.exe']
 vm_ping = ['ping', '-n', '1', '-w', '800', '192.168.116.128']
 disk_per = """typeperf "LogicalDisk(_Total)\% Idle Time" -sc 1""" #查询Windows计数器, 得到IO空闲率
 
 #processes and files
 process_dbx = 'dropbox.exe'
+process_keepass = 'keepass.exe'
 dbx_dir = 'g:\\dropbox'
 
 def execute(command):
@@ -37,7 +39,7 @@ def execute_full(command):
 def ping(vm_ping):
     try:
         output = subprocess.check_output(vm_ping)
-        print('>>>>>Ping响应:', output.decode('gbk').split('\n')[2], '\n>>>>>虚拟机已开机')
+        print('>>>>>Ping response:', output.decode('gbk').split('\n')[2], '\n>>>>>VM is running')
         return 0
     except Exception as e:
         print(e)
@@ -53,17 +55,22 @@ def exists(obj):
 def main():
     while 1:
         isdbx = proc_find(process_dbx)
+        iskeepass = proc_find(process_keepass)
         isdir = exists(dbx_dir)
         disk_percent = execute_full(disk_per)[1].split('"')[7]
         if float(disk_percent) > 95:
-            print('当前IO空闲率: {}%'.format(float(disk_percent)))
+            print('>>>>>Current I/O idle percent: {}%'.format(float(disk_percent)))
             if ping(vm_ping) == 1:
                 execute(vm_ops)
+                continue
+            if not iskeepass:
+                execute(keepass)
+                continue
             if isdir and not isdbx:
                 execute(dbx_start)
-                break
-            elif isdir and isdbx:
-                print('>>>>>Disk mounted and Dropbox is running, exit')
+                continue
+            elif isdir and isdbx and iskeepass:
+                print('>>>>>Disk mounted and Dropbox, Keepass is running, exit')
                 time.sleep(3)
                 break
         time.sleep(3)
