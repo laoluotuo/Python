7e91cac6c657e33b17cd412e0f0e9d1299e8174b
laoluotuo <laoluotuo2500@gmail.com>
2017-02-20 14:18:11 +0800

    Update autostart.py
    修改vm开启探测方式, 放弃ping方式改用vmrun list结果测试; 脚本精简

--- PersonalTools/autostart.py
+++ PersonalTools/autostart.py
@@ -10,9 +10,9 @@ import time
 
 #commands
 vm_ops = ['C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe', 'start', 'E:\\Virtual Machines\\CentOS7.2\\CentOS7.2.vmx']
-dbx_start = ['C:\\Program Files (x86)\\Dropbox\\Client\\Dropbox.exe']
+dropbox = ['C:\\Program Files (x86)\\Dropbox\\Client\\Dropbox.exe']
 keepass = ['D:\\Program Files\\Keepass\\KeePass.exe']
-vm_ping = ['ping', '-n', '1', '-w', '800', '192.168.116.128']
+vm_stat = ['C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe', 'list']
 disk_per = """typeperf "LogicalDisk(_Total)\% Idle Time" -sc 1""" #查询Windows计数器, 得到IO空闲率
 
 #processes and files
@@ -26,7 +26,7 @@ def execute(command):
     """
     exec_cmd = subprocess.Popen(command)
     print('Command {} excuted'.format(command))
-    time.sleep(3)
+    time.sleep(5)
     return exec_cmd
 
 def execute_full(command):
@@ -36,44 +36,33 @@ def execute_full(command):
     exec_cmd = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return ('Command {} excuted'.format(command), exec_cmd.communicate()[0].decode('gbk'))
 
-def ping(vm_ping):
-    try:
-        output = subprocess.check_output(vm_ping)
-        print('>>>>>Ping response:', output.decode('gbk').split('\n')[2], '\n>>>>>VM is running')
-        return 0
-    except Exception as e:
-        print(e)
-        return 1
-
 def proc_find(process):
     proc_lst = [ p.name().lower() for p in psutil.process_iter()]
     return True if process in proc_lst else False
 
-def exists(obj):
-    return os.path.exists(obj)
 
 def main():
     while 1:
         isdbx = proc_find(process_dbx)
         iskeepass = proc_find(process_keepass)
-        isdir = exists(dbx_dir)
+        isdir = os.path.exists(dbx_dir)
+        isvm_run = execute_full(vm_stat)
         disk_percent = execute_full(disk_per)[1].split('"')[7]
         if float(disk_percent) > 95:
             print('>>>>>Current I/O idle percent: {}%'.format(float(disk_percent)))
-            if ping(vm_ping) == 1:
+            if len(isvm_run[1]) < 30:
                 execute(vm_ops)
                 continue
-            if not iskeepass:
+            if isdir and not iskeepass:
                 execute(keepass)
                 continue
             if isdir and not isdbx:
-                execute(dbx_start)
+                execute(dropbox)
                 continue
             elif isdir and isdbx and iskeepass:
-                print('>>>>>Disk mounted and Dropbox, Keepass is running, exit')
+                print('>>>>>VM is running, Disk mounted and Dropbox, Keepass is running, exit')
                 time.sleep(3)
                 break
-        time.sleep(3)
 
 
 if __name__ == '__main__':
